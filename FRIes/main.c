52dda8a14ecbdbf99fa444c54bcfeb05b8102b09
sgreene8 <sgreene@uchicago.edu>
2019-03-30 15:54:34 -0500

    Initial Commit

---
+++ FRIes/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FRIes
+//
+//  Created by Samuel Greene on 3/30/19.
+//  Copyright © 2019 Samuel Greene. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
