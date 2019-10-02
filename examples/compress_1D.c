cd14fdb8f345b7bb8c9c8de26ccb43e0c1af01db
sgreene8 <sgreene@uchicago.edu>
2019-10-02 13:49:34 -0400

    Attempt to fix linking to installed library in examples

--- examples/compress_1D.c
+++ examples/compress_1D.c
@@ -8,13 +8,11 @@
 
 #include <stdio.h>
 #include <FRIES/Ext_Libs/dcmt/dc.h>
-#include <FRIES/compress_utils.h>
 
 int main(int argc, const char * argv[]) {
-    
     // Rn generator
-    mt_struct *rngen_ptr = get_mt_parameter_id_st(32, 521, proc_rank, (unsigned int) time(NULL));
-    sgenrand_mt((uint32_t) time(NULL), rngen_ptr);
+    mt_struct *rngen_ptr = get_mt_parameter_id_st(32, 521, 0, 0);
+    sgenrand_mt(0, rngen_ptr);
     
     printf("random double: %lf\n", genrand_mt(rngen_ptr) / (1. + UINT32_MAX));
     return 0;
