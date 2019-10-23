9785ed2dd0a5ac1fa34012d00b57ec07cbc0f72f
sgreene8 <sgreene@uchicago.edu>
2019-10-23 14:45:22 -0400

    Fix name of basis set for Ne example in script file.

--- examples/run_neon.sh
+++ examples/run_neon.sh
@@ -6,7 +6,7 @@
 # were compiled without MPI support
 
 # Perform i-FCIQMC calculation with ~250k walkers
-../build/FRIES_bin/fciqmc_mol --hf_path Neon_augccpdvz_HF/ --target 250000 --distribution NU --max_dets 300000 --initiator 3
+../build/FRIES_bin/fciqmc_mol --hf_path Neon_augccpvdz_HF/ --target 250000 --distribution NU --max_dets 300000 --initiator 3
 
 # Perform systematic FCI-FRI calculation, compressing Hamiltonian to 260k elements and solution vector to 242k elements
-../build/FRIES_bin/frisys_mol --hf_path Neon_augccpdvz_HF/ --target 260000 --vec_nonz 242000 --mat_nonz 260000 --max_dets 500000 --ini_dir Neon_augccpdvz_HF/
+../build/FRIES_bin/frisys_mol --hf_path Neon_augccpvdz_HF/ --target 260000 --vec_nonz 242000 --mat_nonz 260000 --max_dets 500000 --ini_dir Neon_augccpvdz_HF/
