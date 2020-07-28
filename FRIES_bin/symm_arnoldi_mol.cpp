9c648fedc02db47834fa531fbd4a60c728172c71
sgreene8 <sgreene@uchicago.edu>
2020-07-28 08:33:42 -0600

    Begin implementing symmetric Arnoldi method

---
+++ FRIES_bin/symm_arnoldi_mol.cpp
@@ -0,0 +1,285 @@
+/*! \file
+ *
+ * \brief FRI applied to Arnoldi method for calculating excited-state energies
+ * Rayleigh quotients are constructed from the same random vector on the L & R sides
+ */
+
+#include <cstdio>
+#include <iostream>
+#include <ctime>
+#include <FRIES/io_utils.hpp>
+#include <FRIES/Ext_Libs/dcmt/dc.h>
+#include <FRIES/compress_utils.hpp>
+#include <FRIES/Ext_Libs/argparse.h>
+#include <FRIES/Hamiltonians/molecule.hpp>
+
+static const char *const usage[] = {
+    "symm_arnoldi_mol [options] [[--] args]",
+    "symm_arnoldi_mol [options]",
+    NULL,
+};
+
+int main(int argc, const char * argv[]) {
+    const char *hf_path = NULL;
+    const char *result_dir = "./";
+    const char *ini_path = NULL;
+    unsigned int n_states = 0;
+    unsigned int target_nonz = 0;
+    unsigned int max_n_dets = 0;
+    unsigned int max_iter = 500000;
+    unsigned int n_krylov = 5000;
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_STRING('d', "hf_path", &hf_path, "Path to the directory that contains the HF output files eris.txt, hcore.txt, symm.txt, hf_en.txt, and sys_params.txt"),
+        OPT_INTEGER('m', "vec_nonz", &target_nonz, "Target number of nonzero vector elements to keep after each iteration"),
+        OPT_STRING('y', "result_dir", &result_dir, "Directory in which to save output files"),
+        OPT_INTEGER('p', "max_dets", &max_n_dets, "Maximum number of determinants on a single MPI process."),
+        OPT_STRING('v', "ini_vecs", &ini_path, "Prefix for files containing the vectors with which to initialize the calculation (unless load_dir is provided). Files must have names <trial_vecs>dets<xx> and <trial_vecs>vals<xx>, where xx is a 2-digit number ranging from 0 to (n_states - 1), and be text files."),
+        OPT_INTEGER('k', "num_states", &n_states, "Number of states whose energies will be computed."),
+        OPT_INTEGER('I', "max_iter", &max_iter, "Maximum number of iterations to run the calculation."),
+        OPT_END(),
+    };
+    
+    struct argparse argparse;
+    argparse_init(&argparse, options, usage, 0);
+    argparse_describe(&argparse, "\nRandomized Arnoldi method for calculating excited states", "");
+    argc = argparse_parse(&argparse, argc, argv);
+    
+    if (hf_path == NULL) {
+        fprintf(stderr, "Error: HF directory not specified.\n");
+        return 0;
+    }
+    if (target_nonz == 0) {
+        fprintf(stderr, "Error: target number of nonzero vector elements not specified\n");
+        return 0;
+    }
+    if (max_n_dets == 0) {
+        fprintf(stderr, "Error: maximum number of determinants expected on each processor not specified.\n");
+        return 0;
+    }
+    if (!ini_path) {
+        fprintf(stderr, "Error: path to initial vectors was not specified.\n");
+        return 0;
+    }
+    if (n_states < 2) {
+        fprintf(stderr, "Warning: n_states is 1 or 0. Consider using the power method instead of Arnoldi in this case.\n");
+    }
+    
+    int n_procs = 1;
+    int proc_rank = 0;
+#ifdef USE_MPI
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+#endif
+    
+    // Read in data files
+    hf_input in_data;
+    parse_hf_input(hf_path, &in_data);
+    double eps = in_data.eps;
+    unsigned int n_elec = in_data.n_elec;
+    unsigned int n_frz = in_data.n_frz;
+    unsigned int n_orb = in_data.n_orb;
+    double hf_en = in_data.hf_en;
+    
+    unsigned int n_elec_unf = n_elec - n_frz;
+    unsigned int tot_orb = n_orb + n_frz / 2;
+    
+    uint8_t *symm = in_data.symm;
+    Matrix<double> *h_core = in_data.hcore;
+    FourDArr *eris = in_data.eris;
+    
+    // Rn generator
+    mt_struct *rngen_ptr = get_mt_parameter_id_st(32, 521, proc_rank, (unsigned int) time(NULL));
+    sgenrand_mt((uint32_t) time(NULL), rngen_ptr);
+    
+    // Solution vector
+    unsigned int num_ex = n_elec_unf * n_elec_unf * (n_orb - n_elec_unf / 2) * (n_orb - n_elec_unf / 2);
+    unsigned int spawn_length = max_n_dets / n_procs * num_ex / n_procs / 4;
+    size_t adder_size = spawn_length > 1000000 ? 1000000 : spawn_length;
+    std::function<double(const uint8_t *)> diag_shortcut = [tot_orb, eris, h_core, n_frz, n_elec, hf_en](const uint8_t *occ_orbs) {
+        return diag_matrel(occ_orbs, tot_orb, *eris, *h_core, n_frz, n_elec) - hf_en;
+    };
+    
+    // Initialize hash function for processors and vector
+    std::vector<uint32_t> proc_scrambler(2 * n_orb);
+    
+    if (proc_rank == 0) {
+        for (size_t det_idx = 0; det_idx < 2 * n_orb; det_idx++) {
+            proc_scrambler[det_idx] = genrand_mt(rngen_ptr);
+        }
+        save_proc_hash(result_dir, proc_scrambler.data(), 2 * n_orb);
+    }
+#ifdef USE_MPI
+    MPI_Bcast(proc_scrambler.data(), 2 * n_orb, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+#endif
+    
+    std::vector<uint32_t> vec_scrambler(2 * n_orb);
+    for (size_t det_idx = 0; det_idx < 2 * n_orb; det_idx++) {
+        vec_scrambler[det_idx] = genrand_mt(rngen_ptr);
+    }
+    
+    std::vector<DistVec<double>> sol_vecs;
+    sol_vecs.reserve(n_states);
+    for (uint8_t vec_idx = 0; vec_idx < n_states; vec_idx++) {
+        sol_vecs.emplace_back(max_n_dets, adder_size, n_orb * 2, n_elec_unf, n_procs, diag_shortcut, nullptr, 3, proc_scrambler, vec_scrambler);
+    }
+    size_t det_size = CEILING(2 * n_orb, 8);
+    
+    uint8_t tmp_orbs[n_elec_unf];
+    uint8_t (*orb_indices1)[4] = (uint8_t (*)[4])malloc(sizeof(char) * 4 * spawn_length);
+    
+# pragma mark Set up vectors
+    char vec_path[300];
+    Matrix<uint8_t> *load_dets = new Matrix<uint8_t>(max_n_dets, det_size);
+    for (unsigned int trial_idx = 0; trial_idx < n_states; trial_idx++) {
+        DistVec<double> &curr_sol = sol_vecs[trial_idx];
+        double *load_vals = curr_sol.values();
+        
+        sprintf(vec_path, "%s%02d", ini_path, trial_idx);
+        unsigned int loc_n_dets = (unsigned int) load_vec_txt(vec_path, *load_dets, load_vals, DOUB);
+        sol_vecs[trial_idx].set_curr_vec_idx(2);
+        for (size_t det_idx = 0; det_idx < loc_n_dets; det_idx++) {
+            sol_vecs[trial_idx].add(load_dets[0][det_idx], load_vals[det_idx], 1);
+        }
+        loc_n_dets++; // just to be safe
+        bzero(load_vals, loc_n_dets * sizeof(double));
+        sol_vecs[trial_idx].perform_add();
+    }
+    delete load_dets;
+    
+    char file_path[300];
+    FILE *bmat_file = NULL;
+    FILE *dmat_file = NULL;
+    
+    if (proc_rank == 0) {
+        // Setup output files
+        strcpy(file_path, result_dir);
+        strcat(file_path, "b_matrix.txt");
+        bmat_file = fopen(file_path, "a");
+        if (!bmat_file) {
+            fprintf(stderr, "Could not open file for writing in directory %s\n", result_dir);
+        }
+        
+        strcpy(file_path, result_dir);
+        strcat(file_path, "d_matrix.txt");
+        dmat_file = fopen(file_path, "a");
+        
+        strcpy(file_path, result_dir);
+        strcat(file_path, "params.txt");
+        FILE *param_f = fopen(file_path, "w");
+        fprintf(param_f, "Arnoldi calculation\nHF path: %s\nepsilon (imaginary time step): %lf\nVector nonzero: %u\n", hf_path, eps, target_nonz);
+        fprintf(param_f, "Path for initial vectors: %s\n", ini_path);
+        fprintf(param_f, "Krylov iterations: %u\n", n_krylov);
+        fclose(param_f);
+    }
+    
+    // Parameters for systematic sampling
+    double rn_sys = 0;
+    double loc_norms[n_procs];
+    for (unsigned int vec_idx = 0; vec_idx < n_states; vec_idx++) {
+        if (sol_vecs[vec_idx].max_size() > max_n_dets) {
+            max_n_dets = (unsigned int)sol_vecs[vec_idx].max_size();
+        }
+    }
+    std::vector<size_t> srt_arr(max_n_dets);
+    for (size_t det_idx = 0; det_idx < max_n_dets; det_idx++) {
+        srt_arr[det_idx] = det_idx;
+    }
+    std::vector<bool> keep_exact(max_n_dets, false);
+    
+    for (uint32_t iteration = 0; iteration < max_iter; iteration++) {
+        // Initialize the solution vectors
+        for (uint16_t vec_idx = 0; vec_idx < n_states; vec_idx++) {
+            sol_vecs[vec_idx].copy_vec(2, 0);
+        }
+        if (proc_rank == 0) {
+            printf("Macro iteration %u\n", iteration);
+        }
+        
+        for (uint16_t krylov_idx = 0; krylov_idx < n_krylov; krylov_idx++) {
+            if (proc_rank == 0) {
+                printf("Krylov iteration %u\n", krylov_idx);
+                fflush(dmat_file);
+                fflush(bmat_file);
+            }
+            
+#pragma mark Krylov dot products
+            for (uint16_t vecl_idx = 0; vecl_idx < n_states; vecl_idx++) {
+                DistVec<double> &lvec = sol_vecs[vecl_idx];
+                for (uint16_t vecr_idx = 0; vecr_idx < n_states; vecr_idx++) {
+                    if (vecl_idx == vecr_idx) {
+                        double dprod = lvec.internal_dot(0, 0);
+                        dprod = sum_mpi(dprod, proc_rank, n_procs);
+                        if (proc_rank == 0) {
+                            fprintf(dmat_file, "%.9lf,", dprod);
+                        }
+                    }
+                    else {
+                        DistVec<double> &rvec = sol_vecs[vecr_idx];
+                        double dprod = lvec.multi_dot(rvec.indices(), rvec.occ_orbs(), rvec.values(), rvec.curr_size());
+                        if (proc_rank == 0) {
+                            fprintf(dmat_file, "%.9lf,", dprod);
+                        }
+                    }
+                }
+                if (proc_rank == 0) {
+                    fprintf(dmat_file, "\n");
+                    fprintf(bmat_file, "\n");
+                }
+            }
+            
+# pragma mark Matrix multiplication
+            for (uint16_t vec_idx = 0; vec_idx < n_states; vec_idx++) {
+                DistVec<double> &curr_vec = sol_vecs[vec_idx];
+//                curr_vec.set_curr_vec_idx(0);
+                h_op_offdiag(curr_vec, symm, tot_orb, *eris, *h_core, (uint8_t *)orb_indices1, n_frz, n_elec_unf, 1, -eps);
+                curr_vec.set_curr_vec_idx(0);
+                h_op_diag(curr_vec, 0, 1, -eps);
+            }
+
+#pragma mark Krylov dot products
+            for (uint16_t vecl_idx = 0; vecl_idx < n_states; vecl_idx++) {
+                DistVec<double> &lvec = sol_vecs[vecl_idx];
+                for (uint16_t vecr_idx = 0; vecr_idx < n_states; vecr_idx++) {
+                    double dprod = lvec.internal_dot(0, 0);
+                    dprod = sum_mpi(dprod, proc_rank, n_procs);
+                    if (vecl_idx == vecr_idx) {
+                        if (proc_rank == 0) {
+                            fprintf(dmat_file, "%.9lf,", dprod);
+                        }
+                    }
+                    else {
+                        DistVec<double> &rvec = sol_vecs[vecr_idx];
+                        double dprod = lvec.multi_dot(rvec.indices(), rvec.occ_orbs(), rvec.values(), rvec.curr_size());
+                        if (proc_rank == 0) {
+                            fprintf(dmat_file, "%.9lf,", dprod);
+                        }
+                    }
+                }
+                if (proc_rank == 0) {
+                    fprintf(dmat_file, "\n");
+                    fprintf(bmat_file, "\n");
+                }
+            }
+            
+            size_t new_max_dets = max_n_dets;
+            for (uint16_t vec_idx = 0; vec_idx < n_states; vec_idx++) {
+                DistVec<double> &curr_vec = sol_vecs[vec_idx];
+                curr_vec.add_vecs(0, 1);
+                if (curr_vec.max_size() > new_max_dets) {
+                    new_max_dets = curr_vec.max_size();
+                }
+            }
+        }
+    }
+    
+    if (proc_rank == 0) {
+        fclose(bmat_file);
+        fclose(dmat_file);
+    }
+#ifdef USE_MPI
+    MPI_Finalize();
+#endif
+    return 0;
+}
